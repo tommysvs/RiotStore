008c74dd7622896f5668bb3b751d0cf0fd21865a
Tommy Vega <tommysvs@hotmail.com>
2026-08-01 02:11:18 -0600

    add data and repositories with interfaces and implementations to infrastructure project

---
+++ RiotStore.Infrastructure/Repositories/Implementations/StockRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiotStore.Infrastructure.Repositories.Implementations
+{
+    internal class StockRepository
+    {
+    }
+}
